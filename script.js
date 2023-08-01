5809c6db66affe79bf35124cee28ea0f0553ced1
Praise Layade <layadepraise2005@gmail.com>
2023-08-02 00:28:49 +0100

    Create prompt for first and second user input

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // Create variables to accept first and second user input
+let a = parseInt (prompt("Enter birth year"));
+let b = parseInt (prompt("Enter current year"));
 //Create a subtraction function
 //The subtraction function should return the subtraction between input b and a
 //Invoke subtraction function to run the calculator
