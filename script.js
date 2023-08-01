19bb5605fca9cd3f9956e87d3fa102e0adafd5fd
Praise Layade <layadepraise2005@gmail.com>
2023-08-02 00:25:12 +0100

    Create psuedo code for the simple age calculator

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-const prompt = require("prompt sync")(); 
-
-let a = parseInt(prompt("Insert birth year"));
-let b = parseInt(prompt("Insert current year"));
-
-function subtraction() {
-    return b - a;
-}
-
-console.log(subtraction());
+// Create variables to accept first and second user input
+//Create a subtraction function
+//The subtraction function should return the subtraction between input b and a
+//Invoke subtraction function to run the calculator
