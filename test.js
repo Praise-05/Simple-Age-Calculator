e8567e11b33073786ae2b42256e8e2099dd98936
Praise Layade <layadepraise2005@gmail.com>
2023-08-02 09:19:44 +0100

    The for function should present numbers equal to the array

--- test.js
+++ test.js
@@ -3,6 +3,8 @@ let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 35];
 //Loop through the array
 console.log(array); 
 //The for function should present numbers less than or equal to the input
+for (let i = array; i === array;) {
 
+}
 //The if function should return number with modulus of zero
 //Invoke i to display result
