83752aa2139fdd46c0182994185cffb3f4f0d11d
Praise Layade <layadepraise2005@gmail.com>
2023-08-02 09:16:35 +0100

    Loop through the array

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 // Create an array of numbers
 let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 35];
 //Loop through the array
+console.log(array); 
 //The for function should present numbers less than or equal to the input
+
 //The if function should return number with modulus of zero
 //Invoke i to display result
