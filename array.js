ec0cea6aefe5336ec8e435be07443fd6bd9f9f41
Praise Layade <layadepraise2005@gmail.com>
2023-08-02 15:31:45 +0100

    Add up files

---
+++ array.js
@@ -0,0 +1,16 @@
+// Create an array of numbers
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 35];
+//Loop through the array
+for (let i = [0]; i <= array.length; i++) {
+  let  a = array[i] 
+//The if function should return number with modulus of zero which are even numbers
+  if (a % 2 === 0) {
+//Invoke [i] to display result
+    console. log(a);
+                        
+  }
+    
+}
+
+
+
