e8060d6e36dee98701fba64fc9a5a9ddb8ca6529
Praise Layade <layadepraise2005@gmail.com>
2023-08-02 09:04:34 +0100

    Create an array of numbers

---
+++ test.js
@@ -0,0 +1,6 @@
+// Create an array of numbers
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 35];
+//Loop through the array
+//The for function should present numbers less than or equal to the input
+//The if function should return number with modulus of zero
+//Invoke i to display result
